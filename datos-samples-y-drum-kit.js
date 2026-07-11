a15a76113d2df5e6436c2bb31ec22c416bc86abb
rvcstudio <recordveryclean@gmail.com>
2026-07-11 11:31:44 -0500

    Update datos-samples-y-drum-kit.js

--- datos-samples-y-drum-kit.js
+++ datos-samples-y-drum-kit.js
@@ -1,1 +1,31 @@
-
+    const novedades = [
+    {
+        id: "free-guaracha-sample-pack",
+        titulo: "FREE GUARACHA SAMPLE PACK 2026 🔥 FULL RECOPILACIÓN | Drum Kit, Loops, One Shots, Vocals, MIDI & FX",
+        categoria: "Sample Packs y Drum Kits",
+        resumen: "Este pack reúne una colección profesional de sonidos para productores de música electrónica y guaracha.",
+        imagenCover: "arsenal-img/free-guaracha/guaracha.png",
+        archivoUrl: "samples-y-drum-kits/free-guaracha-sample-pack.html",
+        esExterno: true
+    },
+    {
+        id: "keyscape-one-shots",
+        titulo: "Keyscape One Shots Pack | Colección Profesional",
+        categoria: "Sample Packs y Drum Kits",
+        resumen: "Cada sonido conserva el carácter, la calidez y la expresividad que han convertido a Keyscape en uno de los instrumentos virtuales más utilizados por productores de todo el mundo.",
+        imagenCover: "arsenal-img/free-keyscape-one-shots/keyscape-one-shots.png",
+        archivoUrl: "samples-y-drum-kits/keyscape-one-shots.html",
+        esExterno: true
+    },
+    {
+        id: "dancehall-drum-kit",
+        titulo: "🎹 Dancehall Full Drum Kit | Colección Profesional",
+        categoria: "Sample Packs y Drum Kits",
+        resumen: "Este pack reúne una colección profesional de los mejores sonidos del famoso genero<strong>Dancehall</strong>, cuidadosamente seleccionados y convertidos en un super <strong>Drum Kit</strong> de alta calidad para ofrecer un flujo de trabajo mucho más rápido y creativo.",
+        imagenCover: "arsenal-img/free-dancehall-drum-kit/dancehall-drum-kit.png",
+        archivoUrl: "samples-y-drum-kits/dancehall-drum-kit.html",
+        esExterno: true
+    },       
+                              
+    // ✅ Eliminado el comentario que causaba error
+];
