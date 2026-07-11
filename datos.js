85fe454aeb9eea4fb60ccd1a5b540215c9f9b7c7
rvcstudio <recordveryclean@gmail.com>
2026-07-10 22:18:00 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,1 +1,43 @@
-
+// datos.js
+const novedades = [
+    {
+        id: "compresion-de-voces",
+        titulo: "Compresion de Voces: Tecnicas Pro para Lograr Voces Al Frente en tu Mezcla",
+        categoria: "Mezcla & Procesamiento | Por: RVC Studio",
+        resumen: "La voz es el elemento mas importante de casi cualquier produccion musical.",
+        imagenCover: "imagenes/compresion-de-voces/1176-tipo-fet.jpg",
+        archivoUrl: "posts/compresion-de-voces.html"
+    },
+    {
+        id: "tutorial-4",
+        titulo: "Plantilla para grabar voces en FL Studio con plugins nativos",
+        categoria: "Producción Musical & Workflow | Por: RVC Studio",
+        resumen: "Descubre cómo organizar tus maquetas de audio de manera limpia.",
+        imagenCover: "imagenes/tutorial-4-img/portada.jpg",
+        archivoUrl: "posts/tutorial-4.html"
+    },
+    {
+        id: "orden-maqueta",
+        titulo: "La Importancia del Orden al Hacer una Maqueta: Guía Paso a Paso",
+        categoria: "Producción Musical & Audio | Por: RVC Studio",
+        resumen: "El orden al momento de hacer una maqueta es un factor crucial.",
+        imagenCover: "imagenes/tutorial 1 img 1.jpg",
+        archivoUrl: "posts/orden-maqueta.html"
+    },
+    {
+        id: "configuracion-audio",
+        titulo: "Guía Paso a Paso: Configuración de Audio desde Cero en Ableton Live",
+        categoria: "Configuración & Audio Engineering | Por: RVC Studio",
+        resumen: "Configurar Ableton Live para grabar audio de forma óptima.",
+        imagenCover: "imagenes/hardware config.JPG",
+        archivoUrl: "posts/configuracion-audio.html"
+    },
+    {
+        id: "shortcuts-flstudio",
+        titulo: "Multiplica tu velocidad en FL Studio: Los atajos de teclado",
+        categoria: "Producción Musical & Workflow | Por: RVC Studio",
+        resumen: "¿Cansado de estar haciendo clic en el menú de herramientas cada dos segundos?",
+        imagenCover: "imagenes/cover.jpg",
+        archivoUrl: "posts/shortcuts-flstudio.html"
+    }
+];
